217e5625399a2d7e399e7d7c4f3216aed1325d73
eziogoth <aalachisamir@gmail.com>
2022-05-23 19:56:40 +0200

    calcul de Nkn

--- main.py
+++ main.py
@@ -20,21 +20,41 @@ plt.title("Bernoulli Strategie 1 ")
 #ax.set(xlabel='Loi de bernoulli' , ylabel='Frequency')
 """
 
-import scipy.stats as Stats
+from ipaddress import NetmaskValueError
+from operator import index
+from scipy import stats as Stats
 import numpy as np
 from matplotlib import pyplot as plt
 
 #Stratégie 1:
 #Initialisation
-N=6 #number of patients
+N=15 #number of patients
 Pk=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.48] #probability of each traitement
-K=len(Pk) #number or treatement used 
+K=len(Pk) #number or treatement used
+pk=K*[0] #final probability of each treatment
 Xt=[] #table of Xn
+Nkn=N*[K*[0]]
+LNkn=K*[0] #value of Nkn on n
 for i in range (1,N+1):
     Tn=Stats.randint.rvs(1,K+1)  #Uniform law
+    print("Tn =",Tn)
     Xn=np.random.binomial(1,Pk[Tn-1])  #Bernoulli law
-    Xt.append(Xn)  #list of effecacity{0,1} 
+    Xt.append(Xn)  #list of efficiency{0,1} 
     for j in range (1,K+1):
-        Nkn=[]
-        Nkn.append(np.random.binomial(j,Pk[Tn-1]))  #number of use of treatment
+
+        LNkn[j-1]+= np.random.binomial(1,Pk[j-1]==Pk[Tn-1])  #number of use of all treatments on n
+   
+    print("Nkn=",LNkn)
+
+for j in range(1,K+1):
+    pk[j-1]=LNkn[j-1]/N
+
+Ex=np.mean(Xt)
+
+print(Ex)
+
+
+
+    
+
 
