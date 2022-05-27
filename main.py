5fe3e9988cbc09a301c9c5ef3b01bc3af3ae384d
LifeTakerSimo <lifestaker.simo@gmail.com>
2022-05-27 19:05:11 +0200

    cleanin algo ,

--- main.py
+++ main.py
@@ -36,9 +36,14 @@ def most_frequent(List):
     return num
 
 
+def Sum(L,k,n): #sum of the firt n element of the column L[K] (L is a list of lists)
+    sumCol=0
+    for i in range(0,n):
+        sumCol=sumCol + L[i][k]
+    return sumCol
 
 #Initialisation
-N=5000 #number of patients
+N=1000 #number of patients
 Pk=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.75] #probability of each treatement
 K=len(Pk) #number or treatement used
 Xt=[] #table of Xn
@@ -59,9 +64,8 @@ TE=[] #efficiency of all treatment after use
 Use=numberOfUse(Tn)
 for i in range(N):
     TE.append(Xt[i]*Tn[i])
-Eff=numberOfUse(TE)
-Use=numberOfUse(Tn)
-ratio=[]
+
+#Graphe of Strat 1 
 for i in Use.keys():
     if (not(i in Eff)):
         Eff[i]=0
@@ -74,17 +78,19 @@ plt.grid()
 plt.show()
 Ex=np.mean(Xt)
 print(ratio)
-"""
+
+
 #Treatment of strat 2 : 
-PknS2=[[0 for i in range(K)] for i in range (N)] 
+numberOfMax=[0 for k in range(K)]
 PPkn=[0 for i in range (K)]
+PknS2=[[0 for i in range(K)] for i in range (N)] 
+
 for i in range (1,N+1):
     if(i<K+1):  
         Tnn=i  
         Tn.append(Tnn) 
         Xn=np.random.binomial(1,Pk[i-1])  #Bernoulli law
         Xt.append(Xn)  #list of efficiency{0,1}
-        #print(Xt)
     else:
         Tnn=Stats.randint.rvs(1,K+1)  #Uniform law
         Tn.append(Tnn)
@@ -93,41 +99,32 @@ for i in range (1,N+1):
     for j in range (1,K+1):
         if(j==Tnn):
             PPkn[j-1]+= np.random.binomial(1,Pk[j-1]==Pk[Tnn-1])
-    #print(PPkn)
     NknS2.append(PPkn.copy())
-#print(NknS2)
+
 
 for i in range (1,N+1):
     for j in range (1,K+1):
         if(j==Tn[i-1]):
             Ykn[i-1][j-1]=Xt[i-1]
 
-#print(Ykn)
-
-
-
-def Sum(L,k,n):
-    sumCol=0
-    for i in range(0,n):
-        sumCol=sumCol + L[i][k]
-    return sumCol
-
-
 for i in range (K+1,N+1):
     for j in range (1,K+1):
         sum=Sum(Ykn,j-1,i-1)
         PknS2[i-1][j-1]=round(sum / NknS2[i-1][j-1],3)
-numberOfMax=[0 for k in range(10)]
+
+#Graphe of strat 2
 for i in range (K+1,N+1):
     for j in range(1,K+1):
         if max(PknS2[i-1])==PknS2[i-1][j-1]:
             numberOfMax[j-1]+=1
+
 plt.bar([i for i in range(1,11)],numberOfMax,0.5) 
 plt.title("",fontsize=15)
 plt.ylabel('Nombre de fois ou Pkn est maximal')
 plt.xlabel('Traitement')
 plt.grid()
 plt.show()
+"""
 
-
-#print(PknS2)
+#Treatment of strat 3 : 
+ #calcul de P(K,n )
